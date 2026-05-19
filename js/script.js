ce89832be5efe81128900ac6feab0c135528a009
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-19 18:29:46 +0200

    button

--- js/script.js
+++ js/script.js
@@ -24,3 +24,26 @@ if (contactForm) {
     });
 
 }
+//// new animation update
+//button
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("mouseover", () => {
+
+        button.style.transform = "scale(1.05)";
+        button.style.transition = "0.3s";
+
+    });
+
+    button.addEventListener("mouseout", () => {
+
+        button.style.transform = "scale(1)";
+
+    });
+
+
+
+});
+
