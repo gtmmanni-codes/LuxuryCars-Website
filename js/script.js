e0bf6a6942759a5895e8133d59c1803925bd31aa
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-19 18:30:04 +0200

    cards

--- js/script.js
+++ js/script.js
@@ -47,3 +47,24 @@ buttons.forEach(button => {
 
 });
 
+//cards
+
+const cards = document.querySelectorAll(".card");
+
+
+cards.forEach(card => {
+
+    card.addEventListener("mouseenter", () => {
+
+        card.style.transform = "translateY(-10px)";
+        card.style.transition = "0.3s";
+
+    });
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform = "translateY(0px)";
+
+    });
+
+ });
