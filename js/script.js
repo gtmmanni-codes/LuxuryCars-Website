7f159dcf1c1118fc92cba27e41de8ac9e191f780
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-19 17:55:27 +0200

    JS for contact

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ window.addEventListener("load", () => {
     console.log("Luxury Cars Website Loaded Successfully!");
 
 });
+
+
+
+//contact form
+const contactForm = document.getElementById("contactForm");
+
+if (contactForm) {
+
+    contactForm.addEventListener("submit", function(event) {
+
+        
+
+        alert("Your contact form has been sent successfully!");
+
+        contactForm.reset();
+
+    });
+
+}
